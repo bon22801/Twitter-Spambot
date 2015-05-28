65ee7696d04e1467cfa0267c8dfcedcdc12bf5ca
bon22801 <bon22801@gmail.com>
2015-05-28 19:22:04 -0400

    implemented direct message

--- micro_blogger.rb
+++ micro_blogger.rb
@@ -17,8 +17,22 @@ class MicroBlogger
   end
   
   def dm(target, message)
-    puts "Trying to send #{target} this direct message:"
-    puts message
+    if self.follower?(target)
+      puts "Sent #{message} to #{target}"
+      message = "d @#{target} #{message}"
+      tweet(message)
+    else
+      puts "Cannot DM #{target} because #{target} is not following you."
+    end
+  end
+  
+  def follower?(person)
+    screen_names = @client.followers.collect { |follower| @client.user(follower).screen_name }
+    if screen_names.include?(person)
+      true
+    else
+      false
+    end
   end
   
   def run
@@ -31,6 +45,7 @@ class MicroBlogger
       case command
         when 'q' then puts "Goodbye!"
         when 't' then tweet(parts[1..-1].join(" "))
+        when 'dm' then dm(parts[1], parts[2..-1].join(" "))
         else
            puts "Sorry, I don't know how to #{command}"
       end
